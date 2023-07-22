1b7f4e08bf5bd3193216b99603053a23943deb78
belmirofss <belmirofss@gmail.com>
2023-07-22 20:45:54 -0300

    mocks

---
+++ src/mocks/artists.ts
@@ -0,0 +1,81 @@
+import { SpotifyArtist } from "../types";
+
+export const ARTISTS: SpotifyArtist[] = [
+  {
+    id: "#",
+    name: "Travis Scott",
+    images: [
+      {
+        url: "https://i.scdn.co/image/374b48abf3af2b3fa5940ee838263c63df248a4e",
+        height: 0,
+        width: 0,
+      },
+    ],
+  },
+  {
+    id: "#",
+    name: "Drake",
+    images: [
+      {
+        url: "https://i.scdn.co/image/068ae749ee9825fcecc1a0f82ead09aaa8440228",
+        height: 0,
+        width: 0,
+      },
+    ],
+  },
+  {
+    id: "#",
+    name: "Billie Eilish",
+    images: [
+      {
+        url: "https://i.scdn.co/image/ab6761670000ecd474ab55d1d4b65d7f305d048b",
+        height: 0,
+        width: 0,
+      },
+    ],
+  },
+  {
+    id: "#",
+    name: "Justin Bieber",
+    images: [
+      {
+        url: "https://i.scdn.co/image/ab6761670000ecd470ebb9de96b4fa4811486415",
+        height: 0,
+        width: 0,
+      },
+    ],
+  },
+  {
+    id: "#",
+    name: "Hailee Steinfeld",
+    images: [
+      {
+        url: "https://i.scdn.co/image/ab6761670000ecd471318738f9c0902946995aab",
+        height: 0,
+        width: 0,
+      },
+    ],
+  },
+  {
+    id: "#",
+    name: "Gunna",
+    images: [
+      {
+        url: "https://i.scdn.co/image/b7a71a619849010c6af6267e3ef257cf120611e8",
+        height: 0,
+        width: 0,
+      },
+    ],
+  },
+  {
+    id: "#",
+    name: "Fifth Harmony",
+    images: [
+      {
+        url: "https://i.scdn.co/image/f21328b35ff235a719dfebf64712cf0a02ecf280",
+        height: 0,
+        width: 0,
+      },
+    ],
+  },
+];
---
+++ src/mocks/tracks.ts
@@ -0,0 +1,173 @@
+import { SpotifyTrack } from "../types";
+
+export const TRACKS: SpotifyTrack[] = [
+    {
+        id: '#',
+        name: "SICKO MODE",
+        album: {
+            id: "1",
+            name: '',
+            images: [
+                { 
+                    url: "https://i.scdn.co/image/ab67616d00001e02072e9faef2ef7b6db63834a3",
+                    height: 0,
+                    width: 0,
+                }
+            ]
+        },
+        artists: [
+            {
+                id: '#',
+                name: 'Travis Scott',
+                images: []
+            }
+        ]
+    },
+    {
+        id: '#',
+        name: "goosebumps",
+        album: {
+            id: "#",
+            name: '',
+            images: [
+                { 
+                    url: "https://i.scdn.co/image/ab67616d00001e02f54b99bf27cda88f4a7403ce",
+                    height: 0,
+                    width: 0,
+                }
+            ]
+        },
+        artists: [
+            {
+                id: '#',
+                name: 'Travis Scott',
+                images: []
+            }
+        ]
+    },
+    {
+        id: '#',
+        name: "TV",
+        album: {
+            id: "#",
+            name: '',
+            images: [
+                { 
+                    url: "https://i.scdn.co/image/ab67616d00001e027a4781629469bb83356cd318",
+                    height: 0,
+                    width: 0,
+                }
+            ]
+        },
+        artists: [
+            {
+                id: '#',
+                name: 'Billie Eilish',
+                images: []
+            }
+        ]
+    },
+    {
+        id: '#',
+        name: "back to the moon",
+        album: {
+            id: "#",
+            name: '',
+            images: [
+                { 
+                    url: "https://i.scdn.co/image/ab67616d00001e02017d5e26552345c4b1575b6c",
+                    height: 0,
+                    width: 0,
+                }
+            ]
+        },
+        artists: [
+            {
+                id: '#',
+                name: 'Gunna',
+                images: []
+            }
+        ]
+    },
+    {
+        id: '#',
+        name: "That's My Girl",
+        album: {
+            id: "#",
+            name: '',
+            images: [
+                { 
+                    url: "https://i.scdn.co/image/ab67616d00001e02d03fa6f4e758282b7920b5c8",
+                    height: 0,
+                    width: 0,
+                }
+            ]
+        },
+        artists: [
+            {
+                id: '#',
+                name: 'Fifth Harmony',
+                images: []
+            }
+        ]
+    },
+    {
+        id: '#',
+        name: "Rich Flex",
+        album: {
+            id: "#",
+            name: '',
+            images: [
+                { 
+                    url: "https://i.scdn.co/image/ab67616d00001e0202854a7060fccc1a66a4b5ad",
+                    height: 0,
+                    width: 0,
+                }
+            ]
+        },
+        artists: [
+            {
+                id: '#',
+                name: 'Drake',
+                images: []
+            },
+            {
+                id: '#',
+                name: '21 Savage',
+                images: []
+            }
+        ]
+    },
+    {
+        id: '#',
+        name: "One Dance",
+        album: {
+            id: "#",
+            name: '',
+            images: [
+                { 
+                    url: "https://i.scdn.co/image/ab67616d00001e029416ed64daf84936d89e671c",
+                    height: 0,
+                    width: 0,
+                }
+            ]
+        },
+        artists: [
+            {
+                id: '#',
+                name: 'Drake',
+                images: []
+            },
+            {
+                id: '#',
+                name: 'Wizkid',
+                images: []
+            },
+            {
+                id: '#',
+                name: 'Kyla',
+                images: []
+            }
+        ]
+    }
+]
